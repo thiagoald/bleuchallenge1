e1d5bc1f6a2f5b24ef8e3ac929c2857f559049b8
thiago <thiagoald@gmail.com>
2018-08-04 22:09:19 -0300

    Receive api key and secret (instead of storing with the server)

--- server.go
+++ server.go
@@ -10,50 +10,71 @@ import (
 	"time"
 )
 
-func getPage(uri string) []byte {
-	apiKey := "55251e9d20b4ad8e7e625a406d717b25"
-	apiSecret := "004ccbdf594fedec1d547ff75219f6335242f9ed"
-	nonce := fmt.Sprint(time.Now().Unix() % 1000000000)
-	uri = uri + apiKey + "&nonce=" + nonce
+var etherscanKey = "YCBTHQ1KTEUMYTMYX39YY3U36GSZ3Q2FRP"
+
+func genHash(apiSecret, uri string) (sign string) {
 	hash := hmac.New(sha512.New, []byte(apiSecret))
 	hash.Write([]byte(uri))
-	sign := hex.EncodeToString(hash.Sum(nil))
-	uri = uri + "&apisign=" + sign
-	fmt.Println(uri)
-	resp, _ := http.Get(uri)
-	body, _ := ioutil.ReadAll(resp.Body)
-	return body
+	sign = hex.EncodeToString(hash.Sum(nil))
+	return sign
 }
 
 func balance(w http.ResponseWriter, r *http.Request) {
 	queryValues := r.URL.Query()
-	uri := "https://bleutrade.com/api/v2/account/getbalance?"
+	nonce := fmt.Sprint(time.Now().Unix() % 1000000000)
+	apikey := queryValues.Get("apikey")
 	currency := queryValues.Get("currency")
-	uri += "currency=" + currency
-	text := getPage(uri + "&apikey=")
-	w.Write([]byte(text))
+	apisecret := queryValues.Get("apisecret")
+	uri := "https://bleutrade.com/api/v2/account/getbalance?apikey=" + apikey + "&nonce=" + nonce
+	uri += "&currency=" + currency
+	sign := genHash(apisecret, uri)
+	uri += "&apisign=" + sign
+	resp, _ := http.Get(uri)
+	body, _ := ioutil.ReadAll(resp.Body)
+	fmt.Println(uri)
+	w.Write([]byte(body))
+}
+
+func balances(w http.ResponseWriter, r *http.Request) {
+	queryValues := r.URL.Query()
+	nonce := fmt.Sprint(time.Now().Unix() % 1000000000)
+	apikey := queryValues.Get("apikey")
+	apisecret := queryValues.Get("apisecret")
+	uri := "https://bleutrade.com/api/v2/account/getbalances?apikey=" + apikey + "&nonce=" + nonce
+	sign := genHash(apisecret, uri)
+	uri += "&apisign=" + sign
+	resp, _ := http.Get(uri)
+	body, _ := ioutil.ReadAll(resp.Body)
+	fmt.Println(uri)
+	w.Write([]byte(body))
 }
 
 func withdraw(w http.ResponseWriter, r *http.Request) {
 	queryValues := r.URL.Query()
 	currency := queryValues.Get("currency")
 	quantity := queryValues.Get("quantity")
-	address := "DTBpf4fLrpzSjcYWq6tBqqczznzjHhphgX"
-	comments := "test2"
-	uri := "https://bleutrade.com/api/v2/account/withdraw?"
-	uri += "currency=" + currency
+	address := queryValues.Get("address")
+	apikey := queryValues.Get("apikey")
+	apisecret := queryValues.Get("apisecret")
+	nonce := fmt.Sprint(time.Now().Unix() % 1000000000)
+	uri := "https://bleutrade.com/api/v2/account/withdraw?apikey=" + apikey + "&nonce=" + nonce
+
+	uri += "&currency=" + currency
 	uri += "&quantity=" + quantity
 	uri += "&address=" + address
-	uri += "&comments=" + comments
-	// text := getPage(uri + "&apikey=")
-	// w.Write([]byte(text))
+	sign := genHash(apisecret, uri)
+	uri += "&apisign=" + sign
+	fmt.Println(uri)
+	resp, _ := http.Get(uri)
+	body, _ := ioutil.ReadAll(resp.Body)
+	w.Write([]byte(body))
 }
 
 func main() {
 	http.HandleFunc("/balance", balance)
+	http.HandleFunc("/balances", balances)
 	http.HandleFunc("/withdraw", withdraw)
 	http.Handle("/", http.FileServer(http.Dir("./static")))
-	// http.HandleFunc("/hello", form)
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		panic(err)
 	}
